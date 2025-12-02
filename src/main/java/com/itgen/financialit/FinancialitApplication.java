625fbcb38c047d9e9bcb21348a9ec34bf156e730
eoliveirasobrinho <e.oliveira.sobrinho1987@gmail.com>
2025-12-02 13:45:36 -0300

    first commit

---
+++ src/main/java/com/itgen/financialit/FinancialitApplication.java
@@ -0,0 +1,13 @@
+package com.itgen.financialit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialitApplication.class, args);
+	}
+
+}
